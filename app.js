afcf42ed8037d8cfea6b992b80927887218d9162
Kajalror <krulyan90@gmail.com>
2024-07-13 19:41:50 +0530

    chat app

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 
-// https://member.osat.in:3000
+// https://member.osat.in
+
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const io = require("socket.io")(8080, {
+const io = require("socket.io")(8011, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
+
+
+
+
+
 
 
 //import files
@@ -21,18 +29,60 @@ const Conversations = require("./models/Conversation.js");
 const Messages = require("./models/Messages.js");
 const File = require('./models/File');
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (newMessage) => {
+    io.emit("message", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+});
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", async (newMessage) => {
+    const { _conversationId, userId, message, retailerId } = newMessage;
+    console.log("User connected", newMessage);
+
+    const newMessageEntry = new Messages({
+      _conversationId,
+      userId,
+      message,
+      retailerId,
+    });
+
+    await newMessageEntry.save();
+    io.emit("messages", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+
+
+
 const app = express();
 const cors = require("cors");
 //app Use
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //multer
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 app.use(cors());
-const mongoose = require("mongoose");
+
+
 
 
 app.use((err, req, res, next) => {
@@ -63,53 +113,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-//socket.io
-let users = [];
-io.on("connection", (socket) => {
-  console.log("User connected", socket.id);
- 
-  socket.on("addUser", (userId) => {
-    if (!users.some((user) => user.id === userId)) {
-      users.push({ id: userId, socketId: socket.id });
-    }
-    
-    io.emit("getUsers", users);
-  });
+      
+  
 
-  socket.on(
-    "sendMessage",
-    async ({ userId, retailerId, message, _conversationId, isFile  }) => {
-
-        console.log("user>....", users);
-
-      const retailer = users.find((user) => user?.id === retailerId);
-          console.log("receiver socket", retailer); // receiver
-      const sender = users.find((user) => user.id === userId);
-          console.log("sender socket", sender); // sender
-      const user = await Users.findById(userId);
-          console.log("user find by userId ", user);
-
-      if (retailer && sender) {
-        io.to(retailer.socketId)
-          .to(sender.socketId)
-          .emit("getMessage", {
-            userId,
-            message,
-            _conversationId,
-            retailerId,
-            isFile,
-            user: { id: user._id, fullName: user.fullName, email: user.email },
-          });
-      }
-
-    } );
-
-  socket.on("disconnect", () => {
-    users = users.filter((user) => user.socketId !== socket.id);
-    io.emit("getUsers", users);
-    console.log("User disconnected", socket.id);
-  });
-});
 
 app.get("/", (req, res) => {
   res.send("welcome to my backend");
@@ -201,52 +207,92 @@ app.post("/api/login", async (req, res, next) => {
 
 // how to fetch and create a conversation id
 
+
+
+// app.post("/api/conversation", async (req, res) => {
+//   try {
+//     const { retailerId, userId } = req.body;
+//     let RetailerAndUser = await Conversations.findOne({
+//       retailerId: retailerId,
+//       userId: userId
+//     });
+//     if(RetailerAndUser?._conversationId == null){
+//       RetailerAndUser =  await Conversations.findOne({
+//         retailerId: userId,
+//         userId:  retailerId
+//       });
+//     }
+
+//     if(RetailerAndUser?._conversationId != null){
+//         // console.log("Conversation ID exists:----- ",  RetailerAndUser?._conversationId)
+//       return  res.status(200).json({
+//         message: "Conversation ID exists",
+//         conversationId: RetailerAndUser?._conversationId
+//       });
+//     }
+//     else{
+//       let _conversationId = retailerId + "-" + userId;
+//         const newConversation = new Conversations({
+//           retailerId,
+//           userId,
+//           _conversationId: _conversationId,
+//         });
+//       await  newConversation.save();
+//        //  await console.log("Conversation created:----- ",  newConversation?._conversationId)
+//       return res.status(200).json({
+//         message: "Conversation ID Created",
+//         conversationId: newConversation?._conversationId
+//       });
+//     }
+//   } catch (error) {
+//     console.log(error, "Error");
+//     res.status(500).send("Internal server error");
+//   }
+// });
+
+
+
+
+
 app.post("/api/conversation", async (req, res) => {
   try {
-    const { retailerId, userId, _conversationId } = req.body;
-    if (!_conversationId) {
-      console.log("Invalid conversation ID format");
-      return res.status(400).send("Invalid conversation ID format");
-    }
-    const existingConversation = await Conversations.findOne({
-      _conversationId: _conversationId,
-    });
-    if (existingConversation) {
-      return res.status(200).send("Conversation already exists");
-    }
-    const newConversation = new Conversations({
-      retailerId,
-      userId,
-      _conversationId: _conversationId,
-    });
-    await newConversation.save();
-    console.log("---conversation api's", newConversation);
+    const { retailerId, userId } = req.body;
+    let conversation = await Conversations.findOne({ retailerId, userId }) ||
+                       await Conversations.findOne({ retailerId: userId, userId: retailerId });
 
-    res.status(200).send("Conversation created successfully");
+    if (conversation) {
+      return res.status(200).json({ message: "Conversation ID exists", conversationId: conversation._conversationId });
+    }
+    const _conversationId = `${retailerId}-${userId}`;
+    conversation = new Conversations({ retailerId, userId, _conversationId });
+    await conversation.save();
+    res.status(200).json({ message: "Conversation ID Created", conversationId: conversation._conversationId });
   } catch (error) {
-    console.log(error, "Error");
+    console.log(error);
     res.status(500).send("Internal server error");
   }
 });
+
+
+
 
 app.get("/api/conversation/:conversationId", async (req, res) => {
   try {
     const conversationId = req.params.conversationId;
 
     const [retailerId, userId] = conversationId.split("-");
-    console.log("__user id from conversation api ",userId)
+    // console.log("__user-id from conversation api",userId)
     const conversation = await Conversations.findOne({
       retailerId: retailerId,
     });
-
-    console.log("Conversation Id find :", conversation); //true
+    // console.log("Conversation Id find :", conversation);    //true
 
     if (!conversation) {
-      console.log("Conversation not found for Id:", conversationId);
+      // console.log("Conversation not found for Id:", conversationId);
       return res.status(404).json({ error: "Conversation not found" });
     }
     const receiver = await Users?.findById({ _id: retailerId });
-    console.log(receiver, "receiver found");
+    // console.log(receiver, "receiver found");
     if (!receiver) {
       console.log("Receiver not found for retailerId:", retailerId);
       return res.status(404).json({ error: "Receiver not found" });
@@ -267,107 +313,129 @@ app.get("/api/conversation/:conversationId", async (req, res) => {
 
 //<--- success --->
 
+
 app.post("/api/message", async (req, res) => {
   try {
-    const { _conversationId, userId, message, retailerId = "" } = req.body;
-    console.log("post message ", _conversationId, userId, message, retailerId);
-    if (!userId || !message)
-      return res.status(400).send("please fill all required fields1");
+    const { _conversationId, userId, message, retailerId = " " } = req.body;
+    console.log("Message post message----", req.body);
+
+    if (!userId || !message) {
+      return res.status(400).send("Please fill all required fields.");
+    }
+
     if (_conversationId === "new" && retailerId) {
+      const newConversationId = `${userId}-${retailerId}`;
+      console.log("set up id --- ", newConversationId);
       const newConversation = new Conversations({
-        members: [userId, retailerId],
+        _conversationId: newConversationId,
+        userId,
+        retailerId,
       });
       await newConversation.save();
+
       const newMessage = new Messages({
-        _conversationId: newConversation?._id,
+        _conversationId: newConversationId,
         userId,
         message,
+        retailerId,
       });
+      console.log("Message saved:",await newMessage);
       await newMessage.save();
-      console.log("Message saved:", newMessage);
-      return res.status(200).send(" Message sent successfully");
+      return res.status(201).json({ _conversationId: newConversationId });
     } else if (!_conversationId && !retailerId) {
-      return res.status(400).send("please fill all required fields2");
+      return res.status(400).send("Please fill all required fields.");
     }
-    const newMessage = new Messages({
-      _conversationId,
-      userId,
-      message,
-      retailerId,
-    });
-    await newMessage.save();
-    res.status(200).json("Messages sent successfully");
+
+    // const newMessage = new Messages({
+    //   _conversationId,
+    //   userId,
+    //   message,
+    //   retailerId,
+    // });
+    // // console.log("new message() ", newMessage);
+    // await newMessage.save();
+    // res.status(200).json("Message sent successfully");
   } catch (error) {
     console.log(error, "Error");
+    res.status(500).json({ error: "Internal server error" });
   }
 });
+
 
 app.get("/api/message/:_conversationId", async (req, res) => {
   try {
     const conversationId = req.params._conversationId;
-    console.log(" conversation id for msg : ", conversationId); // true
+
     if (!conversationId) {
-      console.error("Conversation ID is missing");
       return res.status(400).json({ error: "Conversation ID is missing" });
     }
+
     const checkMessages = async (conversationId) => {
-      console.log(`Fetching messages for conversation ID: ${conversationId}`);
       const messages = await Messages.find({ _conversationId: conversationId });
-
-      console.log("check messages type--messages", messages);
-      //this is correct shows only [ array of messages ]
-
+      // console.log("check messages conversationId ", messages);
       if (!messages.length) {
-        console.log("No messages found for this conversation ID");
-        return res
-          .status(404)
-          .json({ error: "No messages found for this conversation ID" });
+        return res.status(404).json({ error: "No messages found for this conversation ID" });
       }
 
-      const messageUserData = await Promise?.all(
+      const messageUserData = await Promise.all(
         messages.map(async (message) => {
           const user = await Users.findById(message?.userId);
+          // console.log("user message user--data ", user);
           return {
+            userId: message.userId,
             id: user?._id,
             email: user?.email,
             fullName: user?.fullName,
-            message: message?.message,
+            message: message.message,
           };
         })
       );
       res.status(200).json(messageUserData);
     };
-    //its shows empty [] 'true condition
 
     if (conversationId === "new") {
       const { userId, retailerId } = req.query;
       if (!userId || !retailerId) {
-        return res
-          .status(400)
-          .json({
-            error:
-              "userId and retailerId query parameters are required for new conversations",
-          });
+        return res.status(400).json({
+          error: "userId and retailerId query parameters are required for new conversations",
+        });
       }
-      const checkConversation = await Conversations.findOne({
-        userId: userId,
-        retailerId: retailerId,
-      });
+      const checkConversation = await Conversations.findOne({ userId, retailerId });
+
+    console.log("check conversation !!!--- ", checkConversation);
+
       if (checkConversation) {
-        console.log("Found existing conversation:", checkConversation);
-        await checkMessages(checkConversation._conversationId);
+        return res.status(200).json({ _conversationId: checkConversation._conversationId });
       } else {
-        console.log("No existing conversation found");
-        return res.status(200).json([]);
+        const newConversationId = `${userId}-${retailerId}`;
+        console.log("check new conversation id  --- ", checkConversation);
+        const newConversation = new Conversations({
+          _conversationId: newConversationId,
+          userId,
+          retailerId,
+        });
+        console.log("new conversation ,,,,api", newConversation);
+        await newConversation.save();
+        return res.status(201).json({ _conversationId: newConversationId });
       }
     } else {
-      await checkMessages(conversationId);
+      const existingConversation = await Conversations.findOne({ _conversationId: conversationId });
+      // console.log("existing conversation", existingConversation);
+      if (existingConversation) {
+        await checkMessages(conversationId);
+      } else {
+        return res.status(404).json({ error: "Conversation not found" });
+      }
     }
   } catch (error) {
     console.log(error, "Error");
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+
+
+
 
 /* 
     app.post("/api/upload", upload.single("file"), (req, res) => {
@@ -403,6 +471,8 @@ app.post('/api/upload', upload.single('file'), async (req, res) => {
 });
 
 
+
+
 app.get("/api/users", async (req, res) => {
   try {
     const users = await Users?.find();
@@ -416,7 +486,7 @@ app.get("/api/users", async (req, res) => {
       })
     );
     res.status(200).json(await usersData);
-    console.log("user form users ", usersData);
+    // console.log("user form users ", usersData);
   } catch (error) {
     console.log(error, "Error");
   }
--- models/Messages.js
+++ models/Messages.js
@@ -2,13 +2,24 @@ const mongoose =require('mongoose');
 const messageSchema =mongoose.Schema({
     _conversationId:{
         type: String,
+        required: true
     },
     userId:{
         type : String,
+        required: true
+    },
+    retailerId: {
+        type: String,
+        required: true
     },
     message: {
         type: String,
-    },      
+        required: true
+    }, 
+    date: {
+        type: Date,
+        default: Date.now
+    } 
 });
 
 const Messages = mongoose.model('Message', messageSchema);
